d0a0dfe6d84a6ebea7d298688036b647c21be918
Tapi33 <artem8554134@gmail.com>
2022-04-24 15:33:10 +0300

    implemented pizza output from dataBase

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Header} from "./components";
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
+import axios from "axios";
 
 
 function App() {
+    const [pizzas, setPizzas] = useState([]);
+    useEffect(() => {
+        axios("http://localhost:3000/db.json").then(({data}) => setPizzas(data.pizzas));
+    },[])
+
     return (
         <div className="wrapper">
             <Header/>
             <div className="content">
                 <Routes>
-                    <Route path={"/"} element={<Home/>}/>
+                    <Route path={"/"} element={<Home items={pizzas}/>}/>
                     <Route path={"/cart"} element={<Cart/>}/>
                 </Routes>
             </div>
