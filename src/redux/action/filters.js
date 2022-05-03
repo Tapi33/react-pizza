a7c2e35df0aa233e00cccd78e17b1fa1d9db94f0
Tapi33 <artem8554134@gmail.com>
2022-05-03 19:24:51 +0300

    create actions

---
+++ src/redux/action/filters.js
@@ -0,0 +1,11 @@
+const setSortBy = (sortType) =>({
+    type:'SET_SORT_BY',
+    payload: sortType,
+});
+
+const setCategory = (categoryIndex) =>({
+    type:'SET_CATEGORY',
+    payload: categoryIndex,
+});
+
+
